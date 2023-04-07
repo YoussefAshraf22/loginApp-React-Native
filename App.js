ca01c53344ad95ab2c2db03f43f58a3f655103b3
Youssef Ashraf <useefashraf22@gmail.com>
2023-04-07 16:01:19 +0200

    app.js

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//npx expo start --dev-client
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import 'expo-dev-client';
+
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
+import ForgetPass from './screens/ForgetPass';
+import Profile from './screens/Profile';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='HomeScreen'component={HomeScreen}/>
+        <Stack.Screen name='LoginScreen'component={LoginScreen}/>
+        <Stack.Screen name='SignupScreen'component={SignupScreen}/>
+        <Stack.Screen name='ForgetPass'component={ForgetPass}/>
+        <Stack.Screen name='Profile'component={Profile}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
