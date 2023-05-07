8c0371ab1451a2bfa283078b59bee1411b77ddae
SaifAshraf22 <saifashraf808@gmail.com>
2023-05-07 22:46:05 +0200

    add favorite & payment screen

---
+++ src/screens/Payment.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Payment = ({navigation}) => {
+  return (
+    <View>
+      <Text>Payment</Text>
+    </View>
+  )
+}
+
+export default Payment
